4672c96d814e422da293936fd3d356a5355d4cee
Lemieli <slemielita@gmail.com>
2021-08-16 17:55:17 -0500

    Added Test Template
    Test for returning sum and throwing exception when adding with a negative number

--- MyProduct/MyProduct.Logic.Tests/MyCalculatorTests.cs
+++ MyProduct/MyProduct.Logic.Tests/MyCalculatorTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace MyProduct.Logic.Tests
 {
@@ -9,14 +10,37 @@ namespace MyProduct.Logic.Tests
         public void Add_TwoNumbers_ReturnsTheSum()
         {
             //Arrange
-            MyCalculator c = new MyCalculator();
-             
+            MyCalculator c = GetNewCalc();
+
             //Act
-            int result = c.Add(1,2);
+            int result = c.Add(1, 2); 
 
             //Assert
             Assert.AreEqual(3, result);
-            
         }
+
+        [TestMethod]
+        public void Add_FirstParamNegative_Throw()
+        {
+            MyCalculator c = GetNewCalc();
+
+            int ANY_POSITIVE = 1;
+            try
+            {
+                c.Add(-1, ANY_POSITIVE);
+            }
+            catch (Exception e)
+            {
+
+                return;
+            }
+
+            Assert.Fail("Expected exception!");
+        }
+
+
+        //Get new instance of calc = maintainability/readability.
+        private static MyCalculator GetNewCalc() => new MyCalculator();
+
     }
 }
--- MyProduct/MyProduct.Logic/MyCalculator.cs
+++ MyProduct/MyProduct.Logic/MyCalculator.cs
@@ -1,9 +1,15 @@
-﻿namespace MyProduct.Logic
+﻿using System;
+
+namespace MyProduct.Logic
 {
     public class MyCalculator
     {
         public int Add(int a, int b)
         {
+            if(a < 0)
+            {
+                throw new Exception("Number cannot be less than zero.");
+            }
             return a + b;
         }
     }
